fe3aca4c6e91fe3fdc89c069321711a0a5db8cf1
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-22 17:17:56 +0530

    added gradient between sections

--- sections/About.jsx
+++ sections/About.jsx
@@ -16,7 +16,7 @@ const About = () => (
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
     >
-      <TypingText title="About Metaversus" textStyles="text-center" />
+      <TypingText title="| About Metaversus" textStyles="text-center" />
       <motion.p
         className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
         variants={fadeIn('up', 'tween', 0.2, 1)}
