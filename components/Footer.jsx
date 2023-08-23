eb3ee09e7b1f059de80cf7434580e80f3cff9ffc
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-23 15:36:12 +0530

    added gradient bg to footer section

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,20 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+import { footerVariants } from '../utils/motion';
+import styles from '../styles';
+import { socials } from '../constants';
+
 const Footer = () => (
-  <footer>
-    footer
-  </footer>
+  <motion.footer
+    variants={footerVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.paddings} py-8 relative`}
+  >
+    <div className="footer-gradient" />
+  </motion.footer>
 );
 
 export default Footer;
