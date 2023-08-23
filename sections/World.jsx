f1af2553cf5c8a860ff195a24cb42d31aa709355
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-23 14:21:46 +0530

    added animated ttitle and text to World sec

--- sections/World.jsx
+++ sections/World.jsx
@@ -1,8 +1,31 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { staggerContainer, fadeIn } from '../utils/motion';
+import { TypingText, TitleText } from '../components';
+
 const World = () => (
-  <section>
-    World section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title="| People on the World" textStyles="text-center" />
+      <TitleText
+        textStyles="text-center"
+        title={
+          <>
+            Track friends around you and invite them to play together in the
+            same world
+          </>
+        }
+      />
+    </motion.div>
   </section>
 );
 
