fcec14ada82a77107fb5248640ad04d1f114f746
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-23 13:31:07 +0530

    added animated title and sub text

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -26,6 +26,13 @@ const GetStarted = () => (
           className="w-[90%] h-[90%] object-contain"
         />
       </motion.div>
+      <motion.div
+        variants={fadeIn('left', 'tween', 0.2, 1)}
+        className="flex-[0.75] flex justify-center flex-col"
+      >
+        <TypingText title="| How Metaversus Works" />
+        <TitleText title={<>Get Started with Just a few clicks</>} />
+      </motion.div>
     </motion.div>
   </section>
 );
