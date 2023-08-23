bfd7b49fa155568d50b40b97e61a9f951fbaf6de
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-23 13:37:31 +0530

    show getting started number

--- components/StartSteps.jsx
+++ components/StartSteps.jsx
@@ -1,6 +1,12 @@
-const StartSteps = () => (
-  <div>
-    start steps
+import styles from '../styles';
+
+const StartSteps = ({ number, text }) => (
+  <div className={`${styles.flexCenter} flex-row`}>
+    <div
+      className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323f5d]`}
+    >
+      <p className="font-bold text-white text-[20px]">0{number}</p>
+    </div>
   </div>
 );
 
